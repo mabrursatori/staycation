c67bebbb8e681f9d7309411e6d65a65724f35300
mabrursatori <mabrursatori@gmail.com>
2020-07-24 16:36:26 +0700

    aplikasi web untuk melayani transaksi penyewaan hotel meupun penginapan yang dibuat dengan Mongo DB, React JS, Node.js dan Express.js

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom'
+import LandingPage from 'pages/LandingPage'
+import Example from 'pages/Example'
+
+import 'assets/scss/style.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Route path="/" component={Example}></Route>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
